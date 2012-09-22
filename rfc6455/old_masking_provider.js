0aa27b481112c685172649dca08198201569b270
das-em <user@server>
2012-09-22 15:08:04 +0200

    removed garbage

--- rfc6455/old_masking_provider.js
+++ rfc6455/old_masking_provider.js
@@ -9,31 +9,19 @@ var MaskingProvider = function(masking_key) {
 	this.offs = 0;
 	this.mask = masking_key;
 
-	// write mask to buffer. Doubling it allows us to 
-	this.m = new Buffer(8);
+	// write mask to buffer
+	this.m = new Buffer(4);
 	this.m.writeInt32BE(this.mask, 0);
-  this.m.writeInt32BE(this.mask, 4);
 }
 
 MaskingProvider.prototype.maskData = function(msgBuff) {
-  var offsMsk = this.m.readInt32BE(this.offs);
-
-  //all bytes that can be masked "fast"
-  var fst = (msgBuff.length - (msgBuff.length % 4));
-  //mask bytes "fast".
-  for(var j = 0; j < fst; j += 4){
-    msgBuff.writeInt32BE(offsMsk ^ msgBuff.readInt32BE(j),j);  
-  }
-  
-  //all overhanging bytes must be masked byte-wise
-  var slw = msgBuff.length % 4;
-	for (var i = fst; i < msgBuff.length; i++) {
+	//console.log("Masking " + msgBuff.length + " bytes data with o ffset " + this.offs);
+	for (var i = 0; i < msgBuff.length; i++) {
 		//get mask octet nr. i
 		var oct = this.m.readInt8((i + this.offs) % 4);
 		//xor single octet.
 		msgBuff.writeInt8(oct ^ msgBuff.readInt8(i), i);
 	}
-  //update offset for next masking.
 	this.offs = (this.offs + msgBuff.length) % 4;
 }
 
